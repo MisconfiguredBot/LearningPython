0d8d64c821607df9e358326b4842ab9960f6a8ba
MisconfiguredBot <curtiscyber@outlook.com>
2025-11-21 14:33:34 -0400

    better calculator functions

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,31 +1,53 @@
 def UserInput():
 
   print("Enter Your First Number:")
-  FirstNumber = input()
+  firstNum = input()
 
   print("Enter Your Operator:")
   Operator = input()
 
   print("Enter Your Second Number:")
-  SecondNumber = input()
+  secondNum = input()
 
-  return FirstNumber, Operator, SecondNumber
+  return firstNum, Operator, secondNum
 
-def math(FirstNumber, Operator, SecondNumber):
-  FirstNumber = float(FirstNumber)
-  SecondNumber = float(SecondNumber)
+def math(firstNum, Operator, secondNum):
+  firstNum = float(firstNum)
+  secondNum = float(secondNum)
   
   if Operator == '+':
-    print("Equals =", FirstNumber + SecondNumber)
+    return firstNum + secondNum
 
   elif Operator == '-':
-    print("Equals =", FirstNumber - SecondNumber)
+    return firstNum - secondNum
 
   elif Operator == '*':
-    print("Equals =", FirstNumber * SecondNumber)
+    return firstNum * secondNum
 
   elif Operator == '/':
-    print("Equals =", FirstNumber / SecondNumber)
+    return firstNum / secondNum
+ 
+result = None 
+
+while True:
+  if result is None:
+    a, op, b = UserInput()
+
+  else:
+    print("Previous result =", result)
+    a = result
+    op = input("Enter Operator: ")
+    b = input("Enter Next Number: ")
+
+  result = math(a, op, b)
+  print("Equals =", result)
+
+  again = input("Do another? (y/n): ")
+  if again.lower() != "y":
+    break
+  
+print("Cyah!")
+
  
 a, op, b = UserInput()
 math(a, op, b)

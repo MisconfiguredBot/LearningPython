d5ca63e7ea94037d95e9464db062fea12238edd5
MisconfiguredBot <curtiscyber@outlook.com>
2025-11-21 13:48:28 -0400

    working calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,3 +1,31 @@
-print("Enter Your First Number:")
-Number = input()
+def UserInput():
 
+  print("Enter Your First Number:")
+  FirstNumber = input()
+
+  print("Enter Your Operator:")
+  Operator = input()
+
+  print("Enter Your Second Number:")
+  SecondNumber = input()
+
+  return FirstNumber, Operator, SecondNumber
+
+def math(FirstNumber, Operator, SecondNumber):
+  FirstNumber = float(FirstNumber)
+  SecondNumber = float(SecondNumber)
+  
+  if Operator == '+':
+    print("Equals =", FirstNumber + SecondNumber)
+
+  elif Operator == '-':
+    print("Equals =", FirstNumber - SecondNumber)
+
+  elif Operator == '*':
+    print("Equals =", FirstNumber * SecondNumber)
+
+  elif Operator == '/':
+    print("Equals =", FirstNumber / SecondNumber)
+ 
+a, op, b = UserInput()
+math(a, op, b)
